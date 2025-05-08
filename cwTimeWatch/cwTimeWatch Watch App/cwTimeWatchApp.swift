af18d671781cdc57c669fe1645447e37fc99c566
JMu <64600882+JettM9104@users.noreply.github.com>
2025-05-08 09:52:26 -0400

    Added watch stuff

---
+++ cwTimeWatch/cwTimeWatch Watch App/cwTimeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  cwTimeWatchApp.swift
+//  cwTimeWatch Watch App
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import SwiftUI
+
+@main
+struct cwTimeWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cwTimeWatch/cwTimeWatch Watch AppTests/cwTimeWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  cwTimeWatch_Watch_AppTests.swift
+//  cwTimeWatch Watch AppTests
+//
+//  Created by Jett Mu on 2025-05-08.
+//
+
+import Testing
+@testable import cwTimeWatch_Watch_App
+
+struct cwTimeWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
